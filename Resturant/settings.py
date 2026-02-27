12b3057eb1ce06aa3ba6f39427842778cc9e7175
Ferdous Reza <ferdousreza631@gmail.com>
2026-02-27 23:05:35 +0600

    deploy

--- Resturant/settings.py
+++ Resturant/settings.py
@@ -91,20 +91,14 @@ WSGI_APPLICATION = 'Resturant.wsgi.application'
 
 # email settings
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'ferdousreza631@gmail.com'
-# EMAIL_HOST_PASSWORD = 'docm hsdz cgdv htsu'
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('ferdousreza631@gmail.com')  
-EMAIL_HOST_PASSWORD = os.getenv('docm hsdz cgdv htsu')  
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+EMAIL_HOST_USER = 'ferdousreza631@gmail.com'
+EMAIL_HOST_PASSWORD = 'docm hsdz cgdv htsu'
+
+
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
